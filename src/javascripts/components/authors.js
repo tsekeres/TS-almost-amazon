2dfb850fbb1409bc2e212bcacc010160c1cc5263
tsekeres <twsekeres@gmail.com>
2021-02-25 18:52:34 -0600

    created authors connections and can print to dom on button push

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -6,7 +6,12 @@ const showAuthors = (array) => {
 
   array.forEach((item) => {
     // FIXME: STUDENTS create cards for your authors
-    document.querySelector('#store').innerHTML += `${item}`;
+    document.querySelector('#store').innerHTML += `<div class="card">
+                            <div class="card-body d-flex flex-column align-items-lg-center" style="height: 100px;">
+                            <h5 class="card-title">${item.last_name}, ${item.first_name}</h5>
+                            <p class="card-text">${item.email}</p>
+                            </div>
+                          </div>`;
   });
 };
 
--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -3,6 +3,8 @@ import domBuilder from '../components/domBuilder';
 import navBar from '../components/navBar';
 import domEvents from '../events/domEvents';
 import navigationEvents from '../events/navigationEvents';
+import getBooks from '../helpers/data/bookData';
+import { showBooks } from '../components/books';
 
 const startApp = () => {
   domBuilder(); // BUILD THE DOM
@@ -10,6 +12,8 @@ const startApp = () => {
   navBar(); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+  // put all books on the DOM
+  getBooks().then((books) => showBooks(books));
 };
 
 export default startApp;
